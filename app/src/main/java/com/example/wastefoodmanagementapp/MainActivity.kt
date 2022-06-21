6eebe78b11dbad7d6ad4258687a9989a97e14313
deveshp007 <deveshp1001@gmail.com>
2022-06-21 20:05:43 +0530

    Added Splash Screen with animation,added a gradient design file, Changed logo

--- app/src/main/java/com/example/wastefoodmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/wastefoodmanagementapp/MainActivity.kt
@@ -1,29 +1,33 @@
 package com.example.wastefoodmanagementapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-
+import android.os.Handler
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var cancelbtn: ImageView
-    lateinit var phonelogin : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        phonelogin = findViewById(R.id.login_with_phone)
-       cancelbtn= findViewById(R.id.cancel_btn)
-        cancelbtn.setOnClickListener {
-            var intent = Intent(this, MainActivity2::class.java)
+
+        // Declaration & Initialization ------------
+        val front_logo: ImageView = findViewById(R.id.front_img)
+
+        // Animation Initialization ----------
+        val img_anim = AnimationUtils.loadAnimation(this, R.anim.slide_anim);
+
+        front_logo.startAnimation(img_anim)
+
+        // Splash Screen code to stay for 3000 milli sec ( 3 sec )
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this@MainActivity, MainActivity2::class.java)
             startActivity(intent)
-        }
-        phonelogin.setOnClickListener {
-
-
-        }
+            finish()
+        }, 3000)
 
     }
 }
