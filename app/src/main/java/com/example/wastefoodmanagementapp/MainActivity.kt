59643b76eabd5938a01f1bff6db3aae15f9755d1
shoaibmoona <shoaibmoona@yahoo.com>
2022-06-21 16:03:01 +0530

    phagwada

--- app/src/main/java/com/example/wastefoodmanagementapp/MainActivity.kt
+++ app/src/main/java/com/example/wastefoodmanagementapp/MainActivity.kt
@@ -10,12 +10,12 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     lateinit var cancelbtn: ImageView
-    lateinit var phonelogin : Button
+    lateinit var phonelogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         phonelogin = findViewById(R.id.login_with_phone)
-       cancelbtn= findViewById(R.id.cancel_btn)
+        cancelbtn = findViewById(R.id.cancel_btn)
         cancelbtn.setOnClickListener {
             var intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
