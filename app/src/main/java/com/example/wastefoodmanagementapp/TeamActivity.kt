2d35454ec079926e046c4bb8a92663aebc60ed3d
shoaibmoona <shoaibmoona@yahoo.com>
2022-06-21 17:31:31 +0530

    phagwada

---
+++ app/src/main/java/com/example/wastefoodmanagementapp/TeamActivity.kt
@@ -0,0 +1,11 @@
+package com.example.wastefoodmanagementapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team)
+    }
+}
