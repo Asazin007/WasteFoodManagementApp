e1288ed21bf483fc533c9eeec66226c0ab47fbf1
shoaibmoona <shoaibmoona@yahoo.com>
2022-06-22 18:29:45 +0530

    Added Strings and Team Activity updated

--- app/src/main/java/com/example/wastefoodmanagementapp/TeamActivity.kt
+++ app/src/main/java/com/example/wastefoodmanagementapp/TeamActivity.kt
@@ -1,11 +1,90 @@
 package com.example.wastefoodmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
 class TeamActivity : AppCompatActivity() {
+
+    // Declarations
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team)
+
+        //-----------------------DrawerLayout And Navigation View---------------------------------
+        drawerLayout = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.navigationView)
+        toggle = ActionBarDrawerToggle(this@TeamActivity, drawerLayout, R.string.Open, R.string.Close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        Interaction with the Drawer Navigation menu items
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.asim -> {
+                    Toast.makeText(this@TeamActivity, "Asim", Toast.LENGTH_SHORT).show()
+                }
+                R.id.shoaib -> {
+                    Toast.makeText(this@TeamActivity, "Shoaib", Toast.LENGTH_SHORT).show()
+                }
+                R.id.devesh -> {
+                    Toast.makeText(this@TeamActivity, "Devesh", Toast.LENGTH_SHORT).show()
+                }
+                R.id.jayveer -> {
+                    Toast.makeText(this@TeamActivity, "Jayveer", Toast.LENGTH_SHORT).show()
+                }
+                R.id.ankit -> {
+                    Toast.makeText(this@TeamActivity, "Ankit", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        when (item.itemId) {
+            R.id.Home -> {
+                Toast.makeText(this@TeamActivity,"You're Home", Toast.LENGTH_SHORT).show()
+                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
+                startActivity(intent)
+            }
+            R.id.chat -> {
+                Toast.makeText(this@TeamActivity,"Chats", Toast.LENGTH_SHORT).show()
+//                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
+//                startActivity(intent)
+            }
+            R.id.noti -> {
+                Toast.makeText(this@TeamActivity,"Notifications", Toast.LENGTH_SHORT).show()
+//                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
+//                startActivity(intent)
+            }
+            R.id.account -> {
+                Toast.makeText(this@TeamActivity,"Account details", Toast.LENGTH_SHORT).show()
+//                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
+//                startActivity(intent)
+            }
+            R.id.team -> {
+                Toast.makeText(this@TeamActivity, "You're already here", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        return true
     }
 }
