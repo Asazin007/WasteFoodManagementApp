0910876b698031520c2ba17deac1acc8e73329ee
shoaibmoona <shoaibmoona@yahoo.com>
2022-06-25 21:26:02 +0530

    Added entry for TeamActivity

--- app/src/main/java/com/example/wastefoodmanagementapp/AsimActivity.kt
+++ app/src/main/java/com/example/wastefoodmanagementapp/AsimActivity.kt
@@ -10,7 +10,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
-class TeamActivity : AppCompatActivity() {
+class AsimActivity : AppCompatActivity() {
 
     // Declarations
     lateinit var drawerLayout: DrawerLayout
@@ -19,12 +19,12 @@ class TeamActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_team)
-
+        setContentView(R.layout.activity_asim)
         //-----------------------DrawerLayout And Navigation View---------------------------------
         drawerLayout = findViewById(R.id.drawerLayout)
         navigationView = findViewById(R.id.navigationView)
-        toggle = ActionBarDrawerToggle(this@TeamActivity, drawerLayout, R.string.Open, R.string.Close)
+        toggle =
+            ActionBarDrawerToggle(this@AsimActivity, drawerLayout, R.string.Open, R.string.Close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
@@ -32,23 +32,22 @@ class TeamActivity : AppCompatActivity() {
         navigationView.setNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.asim -> {
-                    var intent = Intent(this@TeamActivity, AsimActivity::class.java)
-                    startActivity(intent)
+                    Toast.makeText(this@AsimActivity, "You're already here.", Toast.LENGTH_SHORT).show()
                 }
                 R.id.shoaib -> {
-                    var intent = Intent(this@TeamActivity, ShoaibActivity::class.java)
+                    var intent = Intent(this@AsimActivity, ShoaibActivity::class.java)
                     startActivity(intent)
                 }
                 R.id.devesh -> {
-                    var intent = Intent(this@TeamActivity, DeveshActivity::class.java)
+                    var intent = Intent(this@AsimActivity, DeveshActivity::class.java)
                     startActivity(intent)
                 }
                 R.id.jayveer -> {
-                    var intent = Intent(this@TeamActivity, JayActivity::class.java)
+                    var intent = Intent(this@AsimActivity, JayActivity::class.java)
                     startActivity(intent)
                 }
                 R.id.ankit -> {
-                    var intent = Intent(this@TeamActivity, AnkitActivity::class.java)
+                    var intent = Intent(this@AsimActivity, AnkitActivity::class.java)
                     startActivity(intent)
                 }
             }
@@ -66,27 +65,22 @@ class TeamActivity : AppCompatActivity() {
         }
         when (item.itemId) {
             R.id.home_menu -> {
-                Toast.makeText(this@TeamActivity,"You're Home", Toast.LENGTH_SHORT).show()
-                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
+                Toast.makeText(this@AsimActivity,"You're Home", Toast.LENGTH_SHORT).show()
+                var intent = Intent(this@AsimActivity, MainActivity2::class.java)
                 startActivity(intent)
             }
             R.id.chat -> {
-                Toast.makeText(this@TeamActivity,"Chats", Toast.LENGTH_SHORT).show()
-//                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
-//                startActivity(intent)
+                Toast.makeText(this@AsimActivity,"Chats", Toast.LENGTH_SHORT).show()
             }
             R.id.noti -> {
-                Toast.makeText(this@TeamActivity,"Notifications", Toast.LENGTH_SHORT).show()
-//                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
-//                startActivity(intent)
+                Toast.makeText(this@AsimActivity,"Notifications", Toast.LENGTH_SHORT).show()
             }
             R.id.account -> {
-                Toast.makeText(this@TeamActivity,"Account details", Toast.LENGTH_SHORT).show()
-//                var intent = Intent(this@TeamActivity, MainActivity2::class.java)
-//                startActivity(intent)
+                Toast.makeText(this@AsimActivity,"Account details", Toast.LENGTH_SHORT).show()
             }
             R.id.team -> {
-                Toast.makeText(this@TeamActivity, "You're already here", Toast.LENGTH_SHORT).show()
+                var intent = Intent(this@AsimActivity, TeamActivity::class.java)
+                startActivity(intent)
             }
 
         }
