610974282963f64abc75be06b25ef886ca80203a
shoaibmoona <shoaibmoona@yahoo.com>
2022-07-04 02:31:29 +0530

    Modified the Layout for Notification. added Name and Phone number TextView.

--- app/src/main/java/com/example/wastefoodmanagementapp/NotiAdapter.kt
+++ app/src/main/java/com/example/wastefoodmanagementapp/NotiAdapter.kt
@@ -14,7 +14,9 @@ class NotiAdapter(var ctx: Context, var customXml: Int, var donationDetails: Arr
         val layoutInflater: LayoutInflater = LayoutInflater.from(ctx)
         val view: View = layoutInflater.inflate(customXml, null)
         val txt_name: TextView = view.findViewById(R.id.name_row)
+        val txt_number: TextView = view.findViewById(R.id.number_row)
         txt_name.text = donationDetails[position].name
+        txt_number.text = donationDetails[position].phone
         return view
     }
 }
