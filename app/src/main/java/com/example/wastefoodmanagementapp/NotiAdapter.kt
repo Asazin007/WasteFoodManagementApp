7e51b184161dd8971fc386cedb3aa9917756d324
shoaibmoona <shoaibmoona@yahoo.com>
2022-07-04 21:26:05 +0530

    Added View Holder for NotiAdapter.kt

--- app/src/main/java/com/example/wastefoodmanagementapp/NotiAdapter.kt
+++ app/src/main/java/com/example/wastefoodmanagementapp/NotiAdapter.kt
@@ -7,16 +7,27 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
+
+class ViewNotificationHolder(v: View) {
+    val txt_name: TextView = v.findViewById(R.id.name_row)
+    val txt_number: TextView = v.findViewById(R.id.number_row)
+}
 class NotiAdapter(var ctx: Context, var customXml: Int, var donationDetails: ArrayList<donationsModel>):
     ArrayAdapter<donationsModel>(ctx,customXml,donationDetails) {
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val layoutInflater: LayoutInflater = LayoutInflater.from(ctx)
-        val view: View = layoutInflater.inflate(customXml, null)
-        val txt_name: TextView = view.findViewById(R.id.name_row)
-        val txt_number: TextView = view.findViewById(R.id.number_row)
-        txt_name.text = donationDetails[position].name
-        txt_number.text = donationDetails[position].phone
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var view: View
+        var viewNotiHolder: ViewNotificationHolder
+        if (convertView == null) {
+           view = layoutInflater.inflate(customXml, null)
+            viewNotiHolder = ViewNotificationHolder(view)
+            view.tag = viewNotiHolder
+        } else {
+            view = convertView
+            viewNotiHolder = view.tag as ViewNotificationHolder
+        }
+        viewNotiHolder.txt_name.text = donationDetails[position].name
+        viewNotiHolder.txt_number.text = donationDetails[position].phone
         return view
     }
 }
